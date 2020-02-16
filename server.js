b29c243187e5614c3b2a03f259bc82dede734ed9
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-02-16 23:24:45 +0800

    history mode hack

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+var history = require('connect-history-api-fallback');
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
+app.use(history());    
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
-app.listen(port);console.log('server started '+ port);
+app.listen(port);
+console.log('server started '+ port);
+
+
