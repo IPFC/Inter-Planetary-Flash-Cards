a3314589d0e654883ee5bd0740e19c3fbe27399a
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-02-20 15:11:36 +0800

    workbox suppressWarnings is deprecated

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,16 +1,15 @@
 
-/* eslint-disable no-console */
 
 self._precacheManifest = [].concat(self._precacheManifest || []);
-console.log(workbox)
 workbox.core.setCacheNameDetails({prefix: "lumpsum"})
 
-workbox.precaching.suppressWarnings();
+// workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self._precacheManifest, {});
 
 // install new service worker when ok, then reload page.
 self.addEventListener("message", msg =>{
     if (msg.data.action == 'skipWaiting'){
+        // self.clientsClaim()
         self.skipWaiting()
     }
 })
