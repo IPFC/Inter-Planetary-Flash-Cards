e1db2ffa066e8fb4c9a70946c9c28dd7dc02d575
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-02-24 23:01:38 +0800

    Revert "Update service-worker.js"
    This reverts commit fce72f022dffdf5f5dd95831c1a807175a74eabc.

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,7 +7,7 @@ workbox.core.setCacheNameDetails({prefix: "lumpsum"})
 workbox.precaching.precacheAndRoute(self._precacheManifest, {});
 
 // install new service worker when ok, then reload page.
-addEventListener("message", msg =>{
+self.addEventListener("message", msg =>{
     if (msg.data.action == 'skipWaiting'){
 
         skipWaiting()
