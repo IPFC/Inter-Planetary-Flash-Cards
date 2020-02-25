085654fbc35ffbfab9de406a4c5f7bb1cc0bf688
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-02-25 10:30:47 +0800

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,16 +1,15 @@
 
 
-self._precacheManifest = [].concat(self._precacheManifest || []);
-workbox.core.setCacheNameDetails({prefix: "lumpsum"})
-
-// workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self._precacheManifest, {});
-
-this.registration.waiting.postMessage({ type: 'SKIP_WAITING' })
-
 // install new service worker when ok, then reload page.
 self.addEventListener("message", msg =>{
     if (msg.data.action == 'skipWaiting'){
         self.skipWaiting()
     }
 })
+workbox.core.clientsClaim();
+
+self._precacheManifest = [].concat(self._precacheManifest || []);
+workbox.core.setCacheNameDetails({prefix: "lumpsum"})
+
+// workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self._precacheManifest, {});
